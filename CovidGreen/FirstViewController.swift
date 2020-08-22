b70cb4b819985cae0f775a8ab374dfa655390423
KaiKai <kaipeng.tech@gmail.com>
2020-08-22 12:04:34 +1000

    Initial Commit

---
+++ CovidGreen/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CovidGreen
+//
+//  Created by Kaipeng Wu on 22/8/20.
+//  Copyright © 2020 Kaipeng Wu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
